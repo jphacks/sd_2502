ab74d62d169aa860bdd5dbe8ee38bd84c0dcd5f9
lag129 <65485662+lag129@users.noreply.github.com>
2025-10-18 17:04:28 +0900

    latestMessageが無いときの処理を追加

--- nuxt/server/api/message.get.ts
+++ nuxt/server/api/message.get.ts
@@ -2,6 +2,13 @@ export default defineEventHandler(async () => {
   const storage = useStorage();
   const latestMessage = await storage.getItem("latest-message");
 
+  if (!latestMessage) {
+    return {
+      statusCode: 400,
+      message: "No message found",
+    };
+  };
+
   return {
     statusCode: 200,
     message: latestMessage,
